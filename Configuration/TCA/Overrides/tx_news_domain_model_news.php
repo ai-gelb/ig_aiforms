e5bc29eced59e03c22a1c0a3fac6d736f8152ccb
Juan Jaramillo <jaramillo@i-gelb.net>
2024-04-26 11:46:02 +0000

    language

--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -12,7 +12,17 @@ $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['teaser']['config']['fie
     'renderType' => 'aiTextTranslationWizard',
     'IDoThisForYou' => 'I translate this Text.'
 ];
+$GLOBALS['TCA']['tx_news_domain_model_news']['columns']['keywords']['config']['fieldWizard']['aiText'] = [
+    'renderType' => 'aiTextWizard',
+    'aiToRead' => 'title,bodytext',
+    'IDoThisForYou' => 'I give you 5 keywords from  this News title and bodytext. Is separated by comma.'
+];
 
+$GLOBALS['TCA']['tx_news_domain_model_news']['columns']['description']['config']['fieldWizard']['aiText'] = [
+    'renderType' => 'aiTextWizard',
+    'aiToRead' => 'title,bodytext',
+    'IDoThisForYou' => 'I give you a description from  this News. Maximal 160 letters. this description is for SEO.'
+];
 
 
 
@@ -20,7 +30,7 @@ $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['teaser']['config']['fie
 $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['bodytext']['config']['fieldWizard']['aiText'] = [
     'renderType' => 'aiTextRteWizard',
     'aiToRead' => 'title',
-    'IDoThisForYou' => 'I give you a bodytext from  this News title.'
+    'IDoThisForYou' => 'I give you a bodytext from  this News title. Maximal 1000 letters. I must use <h2> and <h3> and <p> and <ul> and <ol> and <li> and <strong>',
 ];
 
 $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['bodytext']['config']['fieldWizard']['aiTextTranslation'] = [
