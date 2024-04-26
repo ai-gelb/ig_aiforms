6e50da3074d58e3d28364c53eb55388a165a18c4
Juan Jaramillo <jaramillo@i-gelb.net>
2024-04-26 12:14:31 +0000

    language

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -4,12 +4,12 @@ defined('TYPO3') || exit();
 
 $GLOBALS['TCA']['sys_file_metadata']['columns']['alternative']['config']['fieldWizard']['aiImageMetadata'] = [
     'renderType' => 'aiImageMetadataWizard',
-    'IDoThisForYou' => 'I give you an alternativ Text for this Image. Maximal 60 letters.',
+    'IDoThisForYou' => 'I give you an alternativ Text for this Image. I Show You what I see.  Maximal 100 letters.',
     'aiPublicFile' => false,
 ];
 
 $GLOBALS['TCA']['sys_file_metadata']['columns']['description']['config']['fieldWizard']['aiImageMetadata'] = [
     'renderType' => 'aiImageMetadataWizard',
-    'IDoThisForYou' => 'I give you an description Text for this Image. Maximal 60 letters.',
+    'IDoThisForYou' => 'I give you an description Text for this Image. Maximal 200 letters.',
     'aiPublicFile' => false
 ];
