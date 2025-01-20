003f2e57f7d6329af58db64d16d63984def0cc5e
Juan Jaramillo <jaramillo@i-gelb.net>
2025-01-20 16:26:00 +0000

    Refactor image alternative field wizard to provide more descriptive instructions

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -4,8 +4,7 @@ defined('TYPO3') || exit();
 
 // $GLOBALS['TCA']['sys_file_metadata']['columns']['alternative']['config']['fieldWizard']['aiImageMetadata'] = [
 //     'renderType' => 'aiImageMetadataWizard',
-//     'iDoThisForYou' => 'I give you an alternativ Text for this Image. I Show You what I see.  Maximal 100 letters.',
-//     'aiPublicFile' => false,
+//     'iDoThisForYou' => 'I give you an alternativ Text for this Image. I Show You what I see.  Maximal 100 letters.'
 // ];
 
 $GLOBALS['TCA']['sys_file_metadata']['columns']['alternative']['config']['fieldWizard']['aiImageMetadata'] = [
@@ -13,12 +12,10 @@ $GLOBALS['TCA']['sys_file_metadata']['columns']['alternative']['config']['fieldW
     'iDoThisForYou' => 'I provide a functional, objective description of the provided image in no more than around 30 words so that someone who could not see it would be able to imagine it. If possible, follow an “object-action-context” framework: The object is the main focus. The action describes what’s happening, usually what the object is doing. The context describes the surrounding environment.
     If there is text found in the image, it is very important that you transcribe all of it, even if it extends the word count beyond 30 words.
     If there is no text found in the image, then there is no need to mention it.
-    I should not begin the description with any variation of “The image”.',
-    'aiPublicFile' => false,
+    I should not begin the description with any variation of “The image”.'
 ];
 
 $GLOBALS['TCA']['sys_file_metadata']['columns']['description']['config']['fieldWizard']['aiImageMetadata'] = [
     'renderType' => 'aiImageMetadataWizard',
-    'iDoThisForYou' => 'I give you an description Text for this Image. Maximal 200 letters.',
-    'aiPublicFile' => false
+    'iDoThisForYou' => 'I give you an description Text for this Image. Maximal 200 letters.'
 ];
